331cd7b8a27fdb00cbe70138454b32bec14311c9
manh.nguyen <parabol123654@gmail.com>
2024-05-30 05:56:33 +0700

    add views, helpers method in controllers

--- __manifest__.py
+++ __manifest__.py
@@ -6,4 +6,12 @@
     'depends': ['base'],
     'installable': True,
     'application': True,
+    'data': [
+        'security/hotel_security.xml',
+        'security/ir.model.access.csv',
+        'views/hotel_menu_views.xml',
+        'views/hotel_booking_views.xml',
+        'views/hotel_customer_views.xml',
+        'views/hotel_room_views.xml',
+    ],
 }
--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,46 @@
-from odoo import http
+
+import functools
+import json
+import logging
+import jwt
+
+import werkzeug.utils
+from werkzeug.exceptions import BadRequest
+
+from odoo import api, http, SUPERUSER_ID, _
+from odoo.exceptions import AccessDenied
+from odoo.http import request, Response
+
+
+from datetime import datetime, timedelta
+
+_logger = logging.getLogger(__name__)
 
 SECRET_KEY = 'your_secret_key_here'
 
-
 class AuthController(http.Controller):
+    
+    def generate_jwt(payload, lifetime=None):
+        # Generates a new JWT token, wrapping information provided by payload (dict)
+        # Lifetime describes (in minutes) how much time the token will be valid
+        if lifetime:
+            payload['exp'] = (datetime.now() + timedelta(minutes=lifetime)).timestamp()
+        return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+    def decode_jwt(token):
+        # Tries to retrieve payload information inside of a existent JWT token (string)
+        # Will throw an error if the token is invalid (expired or inconsistent)
+        return jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+    
+    def authenticate(email, password):
+        if email == 'john@inbox4us.xyz' and password == 'password':
+            return {
+                'username': 'admin',
+                'email': 'john@inbox4us.xyz',
+                'roles': ['admin', 'user']
+            }
+        else:
+            return False
 
     @http.route('/api/register', type='json', auth="none", methods=['POST'], cors='*', csrf=False)
     def register(self, **kwargs):
@@ -13,4 +50,6 @@ class AuthController(http.Controller):
     @http.route('/api/login', type='json', auth='none', methods=['POST'], cors='*', csrf=False)
     def login(self, **kwargs):
         # TODO: Implement user login logic and return JWT token
-        pass
+    
+
+    
